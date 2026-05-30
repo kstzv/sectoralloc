df9dceb1627f15baf5166c0988515a01df65f5cf
kstzv <kstzavertaylo@gmail.com>
2026-05-30 17:40:56 +0300

    Update alloc_memory_header.h

--- alloc_memory_header.h
+++ alloc_memory_header.h
@@ -1,7 +1,7 @@
 #ifndef LIB_H
 #define LIB_H
 
-// Header of the anasin allocator
+// Allocator header
 // Implemented basic constants, alignment formula, error codes,
 // Page types and states, structure for managing a single page,
 // Structure for managing a sector,
@@ -26,19 +26,22 @@ _Static_assert(sizeof(u16) == 2, "u16 error");
 _Static_assert(sizeof(u32) == 4, "u32 error");
 _Static_assert(sizeof(u64) == 8, "u64 error");
 
-#define SIZE_PAGE          4096                   // Size page
-#define SIZE_BLOCK_PAGE    16                     // Size of one page block (in bytes)
-#define NUMBER_BLOCKS_PAGE 256                    // Number of blocks per page
-#define LIMIT_SIZE_BYTES   256                    // The limit for the division of small fragments and large ones
-#define MAX_SIZE_MEM       4096 * 2048            // Maximum number of bytes
-#define MAX_PAGES          MAX_SIZE_MEM/SIZE_PAGE // Maximum number of pages
+#define SIZE_PAGE           4096                   // Size page
+#define SIZE_BLOCK_PAGE     16                     // Size of one page block (in bytes)
+#define NUMBER_BLOCKS_PAGE  256                    // Number of blocks per page
+#define LIMIT_SIZE_BYTES    256                    // The boundary for the division of small fragments and large ones
+#define MAX_SIZE_MEM        4096 * 2048            // Maximum number of bytes
+#define MIN_SIZE_MEM        4096 * 64              // Minimum number of bytes - two sectors
+#define MAX_PAGES           MAX_SIZE_MEM/SIZE_PAGE // Maximum number of pages
+#define DEFAULT_SIZE_SECTOR 32                     // Default sector size
+#define MAX_SECTORS         64                     // Maximum number of sectors
 
-#define BITMAP_BLOCK_IN_PAGE   4  // Bitmap for finding free blocks in the page
+#define BITMAP_BLOCK_IN_PAGE   4  // Bitmap for finding free blocks in a page
 
-// For alignment
+// Alignment according to the formula
 #define ALIGN_UP(x, align) (((x) + (align) - 1) & ~((align) - 1))
 
-// Code of errors
+// Errors codes
 #define ZD_ENOMEM  (-12)
 #define ZD_EINVAL  (-22)
 #define ZD_EBUSY   (-16)
@@ -51,7 +54,7 @@ enum page_type {
     NOT_DEFINED
 };
 
-// Page status - busy, not busy, or partially busy
+// Page status - busy, free, or partial
 enum page_state {
 	PAGE_FREE,
     PAGE_PARTIAL,
@@ -63,50 +66,52 @@ enum page_state {
 // In fragments, tails, or from the current pointer
 enum param_seek {
 	GET_QUICKLY,    // First get memory from the linear current pointer
-    GET_FROM_TAILS, // First get memory to search in the tails of pages
+    GET_FROM_TAILS, // First get the memory to search in the tails of the pages
     DEFAULT         // By default, the allocator will search in fragments first (you can also enter NULL)
 };
 	
 
-// Structure for describing one page
+// Struct for describe page
 struct page {
-	u64 bitmap_seek_free_mem[BITMAP_BLOCK_IN_PAGE]; // Bitmap for finding free page blocks
-	u16 number_this_page;                  			// Current page number
-	enum page_type type_page;    					// Page type - for large or small allocations
-	enum page_state state_page;  					// Busy or not
-	u8 size_max_fragment;                           // Size of the largest fragment (in blocks)
-	u8 size_tail;                                   // Tail size (in blocks)
+	u64 bitmap_seek_free_mem[BITMAP_BLOCK_IN_PAGE]; // Bitmap of searching for free page blocks, if 1 - busy, and 0 - free
+	u8 number_this_page;                  			// Number curr page
+	enum page_type type_page;    					// Type page - for small or large allocatons
+	enum page_state state_page;  					// Busy, free or partial
+	u8 size_max_fragment;                           // Size largest fragment(in blocks)
+	u8 size_tail;                                   // Size tail(in blocks)
 };
 
-// Structure for describing the sector
+// Struct for describe setcor
 struct sector {
 	atomic_flag access;                     		  // Lock for atomic sector capture
 	struct page *first_page;                          // Pointer to the first page of the current sector
-	u32 bitmap_seek_fragment;                         // Bitmap to search for pages where there are fragments (u32 - 4 bytes == 32 bits)
+	u32 bitmap_seek_fragment;                         // Bitmap for searching pages where there are fragments (u32 - 4 bytes == 32 bits)
 	u32 bitmap_seek_tail;                             // Bitmap for finding pages where there is free memory on the tail (u32 - 4 bytes == 32 bits)
-	u8 size_max_fragment;                             // Size of the largest fragment of one of the pages on this sector (in blocks)
+	u8 size_max_fragment;                             // The size of the largest fragment of one of the pages on this sector (in blocks)
 	u8 size_max_tail;                                 // The size of the largest tail of one of the pages in this sector (in blocks)
 };
 	
 	
-// Structure for describing all memory
+// Struct for describe base logic of allocator
 struct mem_alloc {
-	// Fields for browsing and selecting directly from pages
-	u16 number_pages;         // Number of pages
-	u8 *pages_base;           // Pointer to the beginning of memory to use
-	u8 *curr_large_alloc;     // Pointer to current free memory for large allocations
-	u8 *curr_small_alloc;     // Pointer to current free memory for small allocations
-	u16 large_pages_front;    // Counter of the number of consecutive pages involved for large allocations
-	u16 small_pages_front;    // Counter of the number of consecutively allocated pages for small allocations
+	u16 number_pages;                        		// Number pages
+	u8 size_last_sector;                            // Number pages in last sector
+	u16 large_pages_front;                          // Counter of the number of consecutive pages involved for large allocations
+	u16 small_pages_front;                          // Counter of the number of consecutive pages involved for small allocations
 	
-	// For searching and working with sectors
-	u8 number_sectors;                // Number of sectores
+	// For seek and work in sectors
+	u8 number_sectors;                // Number sectors
 	u64 bitmap_fragment_mem_sectors;  // Bitmap for finding sectors with fragmented memory
 	u64 bitmap_tail_mem_sectors;      // Bitmap for finding sectors with tail memory
 	struct sector *first_sector;      // Pointer to the first sector
 	
-	u8 *extra_mem;         // Pointer to reserved balance
-	u16 extra_mem_size;    // Size of reserve
+	u8 *pages_base;                   // Pointer to the beginn memory for allocations
+	u8 *curr_large_alloc;             // Curr ptr to the free memory for large allocations
+	u8 *curr_small_alloc;             // Curr ptr to the free memory for small allocations
+	
+	// Variables indicating remaining memory, it is ignored for now, however, it may be revealed in future versions
+	u8 *extra_mem;         // Ptr to the remainder
+	u16 extra_mem_size;    // Size of remainder
 };
 
 // Initialization function
